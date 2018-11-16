320930808c372b159daf0c2fac0c0688fc4d8cba
Yulya-Yu <your-email-address>
2018-11-16 22:55:19 +0700

    .

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import App from './App'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App />, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
